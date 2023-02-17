9026ad99c47b191e711f3fd1fa37de2aa6cc6dbe
ermias <ermizethi@gmail.com>
2023-02-17 15:13:27 -0800

    bkibk

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle\n";
-	puts(str);
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 
 }
