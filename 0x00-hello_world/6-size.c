151eae57c790f832803c81fbd008d4bcde90649f
ermias <ermizethi@gmail.com>
2023-02-18 17:17:35 -0800

    sixe of types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,17 @@
-#include<stdio.h>
-
+#include <stdio.h>
+/**
+ * main - void
+ *
+ * Description: 'This is to print size of various types'
+ *
+ * Return: 0
+ */
 int main(void)
 {
-        printf("Size of a char: %i byte(s)\n", sizeof(char));
-        printf("Size of a int: %i bytes(s)\n", sizeof(int));
-        printf("Size of a long int: %i bytes(s)\n", sizeof(long int));
-        printf("Size of a long long int: %i bytes(s)\n", sizeof(long long int));
-        printf("size of a float: %i bytes(s)\n", sizeof(float));
-      	return (0);
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of a int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
 }
