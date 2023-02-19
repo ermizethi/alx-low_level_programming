0d67ac3f64ce9365fdfdec82ba72740676b566b0
ermias <ermizethi@gmail.com>
2023-02-18 18:43:05 -0800

    bjhjv

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,12 +13,12 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
-
+i
 	printf("Size of a char: %i byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of an int: %i byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %i byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %i byte(s)\n", (unsigned long)sizeof(d));
-	printf("size of a float: %i byte(s)\n", (unsigned long)sizeof(e));
+	printf("Size of a float: %i byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
 }
 
