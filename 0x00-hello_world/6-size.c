bdfe561d9f7829a0a035b8785ab56a38da8cf35d
ermias <ermizethi@gmail.com>
2023-02-18 16:59:16 -0800

    buf

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(void)
+{
+        printf("Size of a char: %i byte(s)\n", sizeof(char));
+        printf("Size of a int: %i bytes(s)\n", sizeof(int));
+        printf("Size of a long int: %i bytes(s)\n", sizeof(long int));
+        printf("Size of a long long int: %i bytes(s)\n", sizeof(long long int));
+        printf("size of a float: %i bytes(s)\n", sizeof(float));
+      	return (0);
+}
