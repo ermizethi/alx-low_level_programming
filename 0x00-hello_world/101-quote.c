0d7e27c92ba15d700143407510e87e149e270544
ermias <ermizethi@gmail.com>
2023-02-18 20:47:44 -0800

    Erkkab

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include<stdio.h>
+#include<stdlib.h>
 /**
- * main - Entry point
- * Return: Always 1 (Success)
+ * main-void
+ *
+ * Description: 'Printing'
+ *
+ * Return: 1
  */
 int main(void)
 {
-	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19 ";
-        write(1, msg, sizeof(msg));	
+	system("echo and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
