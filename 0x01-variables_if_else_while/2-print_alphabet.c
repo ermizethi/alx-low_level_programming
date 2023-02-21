3c0a715e4c532b70a583b168f2f3bf0553895bd2
ermias <ermizethi@gmail.com>
2023-02-20 16:17:03 -0800

    flyflg

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - void
+ *
+ * Description: 'last digit'
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 'a';
+	int d = 'z';
+
+	while (c <= d)
+	{
+		putchar(c);
+		c++;
+		putchar('\n');
+	}
+	return (0);
+}
