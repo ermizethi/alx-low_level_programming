a0e93f60edfa6df37958f6f115395b6db7c61ecd
ermias <ermizethi@gmail.com>
2023-02-21 05:14:58 -0800

    bak

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - void
+ *
+ * Description: 'exception'
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		printf("%d", a);
+		a++;
+	} putchar('\n');
+	return (0);
+}
