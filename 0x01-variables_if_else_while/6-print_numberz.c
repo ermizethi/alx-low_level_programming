f7cc3d802360d50d313e599a20eb960f0355245b
ermias <ermizethi@gmail.com>
2023-02-21 05:53:55 -0800

    babf

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - void
+ *
+ * Description: 'exception'
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		putchar('0' + a);
+		a++;
+	} putchar('\n');
+	return (0);
+}
