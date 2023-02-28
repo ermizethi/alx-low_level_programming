3ba8f7b053d017a8c36662dd23e0f4963e4d0091
ermias <ermizethi@gmail.com>
2023-02-28 15:34:15 -0800

    jkv

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - main
+ * @str : check
+ * Return : 0
+ */
+void _puts(char *str)
+{
+
+	while (*str != '\0')
+	{
+		_putchar(str);
+		str++;
+	}
+	_putchar('\n');
+}
