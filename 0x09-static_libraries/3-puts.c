8dc62e08d1f8b9d42bc282fa43f9439e6e90a597
ermias <ermizethi@gmail.com>
2023-03-18 14:17:56 -0700

    static library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - void
+ *
+ * Description: 'This is printing text using put function'
+ *
+ * Return: alwaays 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
